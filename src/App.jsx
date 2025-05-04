b41108491bbbb45c08547af89560dc9a6d965c00
Gaurav Pratap Singh <gs492979@gmail.com>
2025-05-04 23:55:31 +0530

    input validation added in bill split form

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,23 @@ import { useState } from "react";
 const initialFriends = [
   {
     userId: 118836,
-    name: "Clark",
-    image: "https://i.pravatar.cc/48?u=118836",
+    name: "Ayushi",
+    image:
+      "https://images.unsplash.com/photo-1624610806703-99c0852c31c0?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGluZGlhbiUyMGdpcmx8ZW58MHx8MHx8fDA%3D?u=118836",
     balance: -7,
   },
   {
     userId: 933372,
-    name: "Sarah",
-    image: "https://i.pravatar.cc/48?u=933372",
+    name: "Vijay",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d0/Vijay_Deverakonda_at_NOTA_pressmeet_%28cropped%29.jpg/500px-Vijay_Deverakonda_at_NOTA_pressmeet_%28cropped%29.jpg?u=933372",
     balance: 20,
   },
   {
     userId: 499476,
-    name: "Anthony",
-    image: "https://i.pravatar.cc/48?u=499476",
+    name: "Megha",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Megha_Akash_%28cropped%29.jpg/500px-Megha_Akash_%28cropped%29.jpg?u=499476",
     balance: 0,
   },
 ];
@@ -93,7 +96,7 @@ function Friend({ friend, onSelection, selectedFriend }) {
   const isSelected = selectedFriend?.userId === friend.userId;
   return (
     <li className={isSelected ? "selected" : ""}>
-      <img src={friend.image} alt={`${friend.name}-image`} />
+      <img className="avatar" src={friend.image} alt={`${friend.name}-image`} />
       <h3>{friend.name}</h3>
       {friend.balance < 0 && (
         <p className="red">
@@ -136,7 +139,11 @@ function FormAddFriend({ onAddNewFriend }) {
       <input
         type="text"
         value={name}
-        onChange={(e) => setName(e.target.value)}
+        onChange={(e) =>
+          setName(
+            e.target.value.charAt(0).toUpperCase() + e.target.value.slice(1)
+          )
+        }
       />
       <label>🌄 Image URL</label>
       <input
@@ -173,17 +180,23 @@ function FormSplitBill({ selectedFriend, onSplitBill }) {
       <input
         type="text"
         value={bill}
-        onChange={(e) => setBill(Number(e.target.value))}
+        onChange={(e) => {
+          const value = e.target.value.trim();
+          const numericValue = Number(value);
+          if (value === "" || isNaN(numericValue)) return;
+          setBill(numericValue);
+        }}
       />
       <label>🧍‍♀️ Your expense</label>
       <input
         type="text"
         value={userExpense}
-        onChange={(e) =>
-          setUserExpense(
-            Number(e.target.value) > bill ? userExpense : Number(e.target.value)
-          )
-        }
+        onChange={(e) => {
+          const value = e.target.value.trim();
+          const numericValue = Number(value);
+          if (value === "" || isNaN(numericValue)) return;
+          setUserExpense(numericValue > bill ? userExpense : numericValue);
+        }}
       />
       <label>👫 {selectedFriend.name}'s expense</label>
       <input type="text" disabled value={friendExpense} />
